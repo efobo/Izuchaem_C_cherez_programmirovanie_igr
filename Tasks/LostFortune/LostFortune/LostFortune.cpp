4fe6b340897c667ba134bd85b0a64cb7013ba8cf
efobo <81191405+efobo@users.noreply.github.com>
2022-08-09 11:35:43 +0300

    Add files via upload

---
+++ Tasks/LostFortune/LostFortune/LostFortune.cpp
@@ -0,0 +1,44 @@
+﻿// Утраченный клад
+// Персонализированная приключенческая игра
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+    const int GOLD_PIECES = 900;
+    int adventures, killed, survivors;
+    string leader;
+    // получаю информацию
+    cout << "Welcome to Lost Fortune\n\n";
+    cout << "Please enter the following for your personalized adventure\n";
+    cout << "Enter a number: ";
+    cin >> adventures;
+
+    cout << "Enter a number smaller than the first: ";
+    cin >> killed;
+
+    survivors = adventures - killed;
+
+    cout << "Enter your last name: ";
+    cin >> leader;
+
+    cout << "\nA brave group of " << adventures << " set out on а quest ";
+    cout << "-- in search of the lost treasure of the Ancient Dwarves. ";
+    cout << "The group was led Ьу that legendary rogue. " << leader << ". \n";
+    cout << "\nAlong the way а band of marauding ogres ambushed the party. ";
+    cout << "All fought bravely under the command of " << leader;
+    cout << ", and the ogres were defeated. but at а cost. ";
+    cout << "Of the adventurers. " << killed << " were vanqui shed. ";
+    cout << "leaving just " << survivors << " in the group.\n";
+    cout << "\nThe party was about to give up all hope. ";
+    cout << "But while laying the deceased to rest. ";
+    cout << "they stumbled upon the buried fortune. ";
+    cout << "So the adventurers split " << GOLD_PIECES << " gold pieces.";
+    cout << leader << " held on to the extra " << (GOLD_PIECES % survivors);
+    cout << " pieces to keep things fair of course.\n";
+    return 0;
+}
